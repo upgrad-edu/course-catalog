9d696cd5b35d950738eb6f9c5922be8fab875492
Srishti Gupta <mail@srishtigupta.in>
2021-04-16 14:22:56 +0530

    overridden Material UI theme with custom colors

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-// Material UI
+// imports for Material UI components
 import { Typography } from "@material-ui/core";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core";
 
-// Styles
-import "./App.css";
+// overriding default Material UI theme
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#d32f2f" /* dark red color */,
+      light: "#ffcdd2" /* light pink color */,
+    },
+    secondary: {
+      main: "#6d9c90" /* turquoise color */,
+    },
+  },
+});
 
 const App = () => {
   return (
-    <Typography variant="h1" gutterBottom>
-      Hello World
-    </Typography>
+    <ThemeProvider theme={theme}>
+      <Typography variant="h1" color="primary" gutterBottom>
+        Hello World
+      </Typography>
+    </ThemeProvider>
   );
 };
 
